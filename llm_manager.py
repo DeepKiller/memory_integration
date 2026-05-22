8338140760e378fd453f8a119b4121fe6349b606
DeepKiller <deep.killer@yandex.by>
2026-05-22 12:52:16 +0300

    context sizes changed

--- llm_manager.py
+++ llm_manager.py
@@ -1,4 +1,4 @@
-from ollama import Client
+from ollama import Client, Options
 import const
 
 @pyscript_compile
@@ -16,4 +16,4 @@ def embed(data):
 
 @pyscript_compile
 def chat(messages):
-    return get_ollama().chat(model=const.CHAT_MODEL, messages=messages, stream=False).message.content
+    return get_ollama().chat(model=const.CHAT_MODEL, messages=messages, stream=False, options=Options(num_predict=128, num_ctx=1024)).message.content
